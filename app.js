8a3e44e4750c8b4c42d9c52cb635e07f22ceb2fa
jk27 <jcarlosmartin27@gmail.com>
2022-02-02 11:00:45 +0000

    Added get and post tours handlers

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
+// DOES => Adds middleware that can modify incoming request data
+app.use(express.json());
+
+/* ---------------------------------------------------
 // DOES => Create routing to determine how app responds to client request (url & http method)
 app.get("/", (req, res) => {
 	res
@@ -12,6 +17,52 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
 	res.send("You can post to this endpoint...");
+});
+--------------------------------------------------- */
+
+// DOES => Read tours data from file
+const tours = JSON.parse(
+	fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+/////////////////////////////////////////////////////////// GET TOURS ROUTE
+// DOES => Create routing to determine how app responds to client request (url & http method)
+app.get("/api/v1/tours", (req, res) => {
+	// Route handler sends back all tours when user hits tours resource URL
+	res.status(200).json({
+		status: "success",
+		results: tours.length,
+		data: {
+			tours, // tours: tours
+		},
+	});
+});
+
+/////////////////////////////////////////////////////////// POST TOURS ROUTE
+
+app.post("/api/v1/tours", (req, res) => {
+	// Route handler sends data from client to the server
+	// console.log(req.body);
+
+	// DOES => Creates new ID by getting id from previous entry and then adding 1
+	const newId = tours[tours.length - 1].id + 1;
+	// DOES => Creates new tour using newId and getting the body from the request
+	const newTour = Object.assign({ id: newId }, req.body);
+
+	// DOES => Adds newTour to the tours array overwriting the file
+	tours.push(newTour);
+	fs.writeFile(
+		`${__dirname}/dev-data/data/tours-simple.json`,
+		JSON.stringify(tours),
+		err => {
+			res.status(201).json({
+				status: "success",
+				data: {
+					tour: newTour,
+				},
+			});
+		}
+	);
 });
 
 const port = 8000;
