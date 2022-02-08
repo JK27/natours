ce18c31a4a4bc6043e2d92cbfe3732993f56b9bc
jk27 <jcarlosmartin27@gmail.com>
2022-02-08 08:26:48 +0000

    Set up config.env

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 /////////////////////////////////////////////////////////// MIDDLEWARES
+// DOES => Only use morgan when on development
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 // DOES => Adds middleware that can modify incoming request data
-app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
 	next();
 });
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
 
 /////////////////////////////////////////////////////////// START SERVER
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
 	console.log(`App running on port ${port}...`);
 });
