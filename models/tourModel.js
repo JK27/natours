03210f04af28d5f1f78421a3b606fd0c61db3eab
jk27 <jcarlosmartin27@gmail.com>
2022-02-08 14:46:48 +0000

    Updated Tour Schema adding more fields

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,21 +2,74 @@ const mongoose = require("mongoose");
 
 ////////////////////////////////////////////////////////// TOUR SCHEMA
 const tourSchema = new mongoose.Schema({
+	////////////////////////////////////////// NAME
 	name: {
 		type: String,
 		// DOES => Sets property as required and adds error message if false
-		required: [true, "A tour must have a name"],
+		required: [true, "Tour must have a name"],
 		unique: true,
+		// DOES => Removes white space at begining and end of string
+		trim: true,
 	},
-	rating: {
+	////////////////////////////////////////// DURATION
+	duration: {
+		type: Number,
+		required: [true, "Tour must have a duration"],
+	},
+	////////////////////////////////////////// MAX GROUP SIZE
+	maxGroupSize: {
+		type: Number,
+		required: [true, "Tour must have a group size"],
+	},
+	////////////////////////////////////////// DIFFICULTY
+	difficulty: {
+		type: String,
+		required: [true, "Tour must have a difficulty"],
+	},
+	////////////////////////////////////////// RATINGS AVERAGE
+	ratingsAverage: {
 		type: Number,
 		default: 4.5,
 	},
+	////////////////////////////////////////// RATINGS QUANTITY
+	ratingsQuantity: {
+		type: Number,
+		default: 0,
+	},
+	////////////////////////////////////////// PRICE
 	price: {
 		type: Number,
-		// DOES => Sets property as required and adds error message if false
-		required: [true, "A tour must have a price"],
+		required: [true, "Tour must have a price"],
 	},
+	////////////////////////////////////////// PRICE DISCOUNT
+	priceDiscount: {
+		type: Number,
+	},
+	////////////////////////////////////////// SUMMARY
+	summary: {
+		type: String,
+		required: [true, "Tour must have a description"],
+		trim: true,
+	},
+	////////////////////////////////////////// DESCRIPTION
+	description: {
+		type: String,
+		trim: true,
+	},
+	////////////////////////////////////////// COVER IMAGE
+	imageCover: {
+		type: String,
+		required: [true, "Tour must have a cover image"],
+	},
+	////////////////////////////////////////// IMAGES
+	images: [String],
+	////////////////////////////////////////// CREATED AT
+	createdAt: {
+		type: Date,
+		default: Date.now(), // Mongoose automatically converts it to current date
+	},
+	////////////////////////////////////////// START DATES
+	startDates: [Date],
 });
 
 // DOES => Creates a Tour model following tourSchema
