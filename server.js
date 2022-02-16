76f7aef68310d357a8e06964f1ae0e00db71f52f
jk27 <jcarlosmartin27@gmail.com>
2022-02-16 14:09:59 +0000

    Implemented unhandle rejections and uncaught exceptions error handlers

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+/////////////////////////////////////////////////////////// UNCAUGHT EXCEPTIONS
+process.on("uncaughtException", err => {
+	console.log("UNCAUGHT EXCEPTION! 💥 SHUTTING DOWN...");
+	console.log(err.name, err.message);
+	// DOES => Gives the server time to finish all pending requests before exiting the application
+	process.exit(1);
+});
+
 /////////////////////////////////////////////////////////// DOTENV
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
@@ -14,6 +22,16 @@ mongoose.connect(DB).then(() => console.log("DB connection succesful ✅"));
 
 /////////////////////////////////////////////////////////// START SERVER
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`App running on port ${port}...`);
+});
+
+/////////////////////////////////////////////////////////// UNHANDLE REJECTIONS
+process.on("unhandledRejection", err => {
+	console.log("UNHANDLED REJECTION! 💥 SHUTTING DOWN...");
+	console.log(err.name, err.message);
+	// DOES => Gives the server time to finish all pending requests before exiting the application
+	server.close(() => {
+		process.exit(1);
+	});
 });
