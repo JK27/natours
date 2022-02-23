8cf229728f08138d44c956e22a618a553eaa1b85
jk27 <jcarlosmartin27@gmail.com>
2022-02-23 08:20:06 +0000

    Implemented cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,19 @@ const signToken = id => {
 /////////////////////////////////////////////////////////// CREATE SEND TOKEN
 const createSendToken = (user, statusCode, res) => {
 	const token = signToken(user._id);
+	///////////////////////////////////// COOKIES
+	const cookieOptions = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+		),
+		httpOnly: true, // Avoids browser to be able to access or modify cookies
+	};
+	// DOES => Cookie will only be sent in secure sessions
+	if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+	res.cookie("jwt", token, cookieOptions);
+	// DOES => Removes password from the output
+	user.password = undefined;
 
 	res.status(statusCode).json({
 		status: "success",
