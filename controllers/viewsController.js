a2dde2fc51f277b70444acf70e8fdc84399aaa9b
jk27 <jcarlosmartin27@gmail.com>
2022-03-03 14:30:25 +0000

    Created card containers in overview template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,19 @@
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
+
 /////////////////////////////////////////////////////////// GET OVERVIEW
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
+	// DOES => 1) Gets tour data from collection.
+	const tours = await Tour.find();
+
+	// DOES => 2) Builds the template.
+	// DOES => 3) Renders the template using data from step 1.
+
 	res.status(200).render("overview", {
 		title: "Exciting tours for adventurous people",
+		tours,
 	});
-};
+});
 /////////////////////////////////////////////////////////// GET OVERVIEW
 exports.getTour = (req, res) => {
 	res.status(200).render("tour", {
