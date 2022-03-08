7416386a624a3513c4c8e1884ec1665e65403930
jk27 <jcarlosmartin27@gmail.com>
2022-03-08 17:21:10 +0000

    Implemented get, update and delete bookings functionality

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -6,11 +6,20 @@ const router = express.Router();
 
 // DOES => Middleware function protecting all routes coming after it. User must be authenticated in order to be able to access these routes.
 router.use(authController.protect);
+//////////////////////////////////////////// CHECKOUT SESSION ROUTE
+router.get("/checkout-session/:tourId", bookingController.getCheckoutSession);
+//////////////////////////////////////////// ROOT ROUTES
+router.use(authController.restrictTo("admin", "lead-guide"));
+router
+	.route("/")
+	.get(bookingController.getAllBookings)
+	.post(bookingController.createBooking);
 
-router.get(
-	"/checkout-session/:tourId",
-	authController.protect,
-	bookingController.getCheckoutSession
-);
+//////////////////////////////////////////// ID ROUTES
+router
+	.route("/:id")
+	.get(bookingController.getBooking)
+	.patch(bookingController.updateBooking)
+	.delete(bookingController.deleteBooking);
 
 module.exports = router;
