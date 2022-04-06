c2c6f61671debb5214ddc191d68adb9b52bcf8d3
jk27 <jcarlosmartin27@gmail.com>
2022-04-06 14:13:10 +0100

    Fixed stripe webhooks to work on current version

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -21,16 +21,23 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 		cancel_url: `${req.protocol}://${req.get.host}/tour/${tour.slug}`,
 		customer_email: req.user.email,
 		client_reference_id: req.params.tourId,
-		display_items: [
+		mode: "payment",
+		line_items: [
 			{
-				name: `${tour.name} Tour`,
-				description: tour.summary,
-				images: [
-					`${req.protocol}://${req.get("host")}/img/tours/${tour.imageCover}`,
-				],
-				amount: tour.price * 100, // Stripe expects amount to be in cents.
-				currency: "usd",
 				quantity: 1,
+				price_data: {
+					currency: "usd",
+					unit_amount: tour.price * 100,
+					product_data: {
+						name: `${tour.name} Tour`,
+						description: tour.summary,
+						images: [
+							`${req.protocol}://${req.get("host")}/img/tours/${
+								tour.imageCover
+							}`,
+						],
+					},
+				},
 			},
 		],
 	});
@@ -56,7 +63,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
 	const tour = session.client_reference_id;
 	const user = (await User.findOne({ email: session.customer_email })).id;
-	const price = session.display_items[0].amount / 100;
+	const price = session.amount_total / 100;
 	await Booking.create({ tour, user, price });
 };
 
