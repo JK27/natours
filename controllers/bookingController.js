d2079c320a586f4a3187cd39c5aa43fe32a84742
jk27 <jcarlosmartin27@gmail.com>
2022-04-06 13:59:57 +0100

    Fixed stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -21,11 +21,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 		cancel_url: `${req.protocol}://${req.get.host}/tour/${tour.slug}`,
 		customer_email: req.user.email,
 		client_reference_id: req.params.tourId,
-		line_items: [
+		display_items: [
 			{
 				name: `${tour.name} Tour`,
 				description: tour.summary,
-				images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+				images: [
+					`${req.protocol}://${req.get("host")}/img/tours/${tour.imageCover}`,
+				],
 				amount: tour.price * 100, // Stripe expects amount to be in cents.
 				currency: "usd",
 				quantity: 1,
@@ -54,7 +56,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
 	const tour = session.client_reference_id;
 	const user = (await User.findOne({ email: session.customer_email })).id;
-	const price = session.line_items[0].amount / 100;
+	const price = session.display_items[0].amount / 100;
 	await Booking.create({ tour, user, price });
 };
 
@@ -71,7 +73,7 @@ exports.webhookCheckout = (req, res, next) => {
 		return res.status(400).send(`Webhook error: ${err.message}`);
 	}
 
-	if (event.type === "checkout.session.complete")
+	if (event.type === "checkout.session.completed")
 		createBookingCheckout(event.data.object);
 
 	res.status(200).json({ received: true });
